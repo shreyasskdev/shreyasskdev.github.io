173d0b55a785420583a7c5d5a5854cdd9204bc10
Shreyas sk <shreyassk.dev@gmail.com>
2024-07-05 22:59:20 +0530

    Refreshed UI

--- main.js
+++ main.js
@@ -1,53 +1,61 @@
-class typeWriter {
-	constructor() {
-		
-        this.typeElement = document.querySelector(".discription .type");
-        this.words = ["programming", "3d art", "drawing", "crafting"];
-        this.wordIndex = 0;
-        this.txt = "";
-        this.isDeleting = false;
-        // this.typeSpeed = 500;
-        this.wait = 2000;
+document.addEventListener('DOMContentLoaded', function() {
 
-
-	}
-    type() {
-
-        let currentWord = this.wordIndex % this.words.length;
-        let fulltxt = this.words[currentWord];
-
-        if (this.isDeleting) {
-            // remove
-            this.txt = fulltxt.substring(0, this.txt.length -1);
-        } else {
-            // add
-            this.txt = fulltxt.substring(0, this.txt.length +1);
-        }
-
-        this.typeElement.textContent = "my hobies are " + this.txt
-        // console.log(this.txt);
-
-        this.typeSpeed = 300
-
-        if (this.isDeleting) {
-            this.typeSpeed /= 3;
-        }    
-
-        // if word is complete
-        if (!this.isDeleting && this.txt === fulltxt) {
-            this.isDeleting = true;
-            this.typeSpeed = this.wait;
-        }else if (this.isDeleting && this.txt === "") {
-            this.typeSpeed = 1000;
-            this.isDeleting = false;
-            this.wordIndex++;
-        }
-
-
-        setTimeout(() => this.type(), this.typeSpeed);
+    var ripple_wrap = document.querySelector('.ripple-wrap'),
+        rippler = document.querySelector('.ripple'),
+        finish = false,
+        storedcontent,
+        monitor = function(el) {
+          var computed = window.getComputedStyle(el, null),
+              borderwidth = parseInt(computed.getPropertyValue('border-left-width'));
+          console.log(borderwidth)
+          if (!finish && borderwidth >= 1500) {
+            el.style.WebkitAnimationPlayState = "paused";
+            el.style.animationPlayState = "paused";
+            swapContent();
+          }
+          if (finish) {
+            el.style.WebkitAnimationPlayState = "running";
+            el.style.animationPlayState = "running";
+            return;
+          } else {
+            window.requestAnimationFrame(function() {monitor(el)});
+          }
+        };
+    
+    storedcontent = document.getElementById('content-2');
+    
+    function handleAnimationEnd() {
+      ripple_wrap.classList.remove('goripple');
     }
-}
+  
+    rippler.addEventListener("webkitAnimationEnd", handleAnimationEnd);
+    rippler.addEventListener("oAnimationEnd", handleAnimationEnd);
+    rippler.addEventListener("msAnimationEnd", handleAnimationEnd);
+    rippler.addEventListener("mozAnimationEnd", handleAnimationEnd);
+    rippler.addEventListener("animationend", handleAnimationEnd);
+  
+    document.body.addEventListener('click', function(e) {
+      if (e.target.tagName.toLowerCase() === 'a') {
+        rippler.style.left = e.clientX + 'px';
+        rippler.style.top = e.clientY + 'px';
+        e.preventDefault();
+        finish = false;
+        ripple_wrap.classList.add('goripple');
+        window.requestAnimationFrame(function() {monitor(rippler)});
+      }
+    });
+    
 
-
-const typewriter = new typeWriter();
-typewriter.type();
+    function swapContent() {
+      if (storedcontent.style.display == "none") {
+        storedcontent.style.display = "flex";
+      } else {
+        storedcontent.style.display = "none";
+      }
+      
+      setTimeout(function() {
+        finish = true;
+      }, 10);
+    }
+    
+  });
